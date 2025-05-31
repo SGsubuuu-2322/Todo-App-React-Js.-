eba58d5398a1f9b7b1b837c668c43a7ed4e92727
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2025-05-31 19:45:59 +0530

    Setup and initialized the project...

---
+++ TODO-APP/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="w-full h-[100vh] bg-primary-bg flex items-center justify-center">
+        <p className="read-the-docs bg-secondary-bg text-primary-text inline-block w-[200px] rounded-2xl p-2.5 cursor-pointer active:scale-105 duration-300">
+          Click on the Vite and React logos to learn more
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
