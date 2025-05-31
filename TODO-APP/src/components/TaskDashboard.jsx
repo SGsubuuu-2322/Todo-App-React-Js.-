e1010e7a05097b485d235240d7ecb2455c5c9671
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2025-06-01 03:13:23 +0530

    Impleted the todo details/overview section...

--- TODO-APP/src/components/TaskDashboard.jsx
+++ TODO-APP/src/components/TaskDashboard.jsx
@@ -1,11 +1,37 @@
 import React from "react";
 
 const TaskDashboard = ({ tasks }) => {
-  console.log(tasks);
+  const overdueCount = tasks?.filter((task) => {
+    if (!task.taskDueDate) return false;
+
+    const dueDate = new Date(task.taskDueDate);
+    const today = new Date();
+    dueDate.setHours(0, 0, 0, 0);
+    today.setHours(0, 0, 0, 0);
+
+    return !task.completion && dueDate < today;
+  }).length;
   return (
     <>
       <div className="dashboard-container w-full p-3">
-        <div className="dashboard-wrapper bg-primary-text"></div>
+        <div className="dashboard-wrapper p-2 flex justify-center items-center gap-2 mt-5">
+          <div className="task-details-box bg-secondary-bg w-[10rem] flex flex-col items-center justify-center shadow-xl/30 shadow-border-shadow rounded-3xl py-2">
+            {tasks?.length}
+            <span>Total Tasks</span>
+          </div>
+          <div className="task-details-box bg-secondary-bg w-[10rem] flex flex-col items-center justify-center shadow-xl/30 shadow-border-shadow rounded-3xl py-2">
+            {tasks?.filter((task) => task.completion == true).length}
+            <span>Completed</span>
+          </div>
+          <div className="task-details-box bg-secondary-bg w-[10rem] flex flex-col items-center justify-center shadow-xl/30 shadow-border-shadow rounded-3xl py-2">
+            {tasks?.filter((task) => task.completion != true).length}
+            <span>Pending</span>
+          </div>
+          <div className="task-details-box bg-secondary-bg w-[10rem] flex flex-col items-center justify-center shadow-xl/30 shadow-border-shadow rounded-3xl py-2">
+            {overdueCount}
+            <span>Overdue</span>
+          </div>
+        </div>
       </div>
     </>
   );
