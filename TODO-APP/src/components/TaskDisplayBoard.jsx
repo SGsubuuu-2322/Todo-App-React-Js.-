c71778202c713d625f38548bf3a9194a7daaacde
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2025-06-01 23:14:41 +0530

    Modified some codes...

--- TODO-APP/src/components/TaskDisplayBoard.jsx
+++ TODO-APP/src/components/TaskDisplayBoard.jsx
@@ -21,10 +21,10 @@ const TaskDisplayBoard = ({ tasks, setTasks }) => {
   return (
     <>
       <div className="task-display-board-container w-full px-3 py-2 mt-4">
-        <div className="display-board-wrapper flex gap-2.5 justify-center items-center flex-wrap">
+        <div className="display-board-wrapper flex gap-3.5 justify-center items-center flex-wrap">
           {tasks.map((task, index) => (
             <div
-              className="task-contaienr bg-primary-text text-primary-bg text-lg font-medium px-3 py-2 rounded-lg flex flex-col justify-center items-center gap-4 hover:scale-105 duration-200 cursor-pointer shadow-xl/30 shadow-white"
+              className="task-contaienr bg-primary-text text-primary-bg text-lg font-medium px-3 py-3.5 rounded-lg flex flex-col justify-center items-center gap-4 hover:scale-105 duration-200 cursor-pointer shadow-xl/30 shadow-white"
               key={index}
             >
               <div className="task-box flex justify-center items-center gap-4">
@@ -55,13 +55,13 @@ const TaskDisplayBoard = ({ tasks, setTasks }) => {
                 </div>
               </div>
               <div className="task-dtls-box flex gap-1.5">
-                <span className="task-priority px-2 py-1 bg-secondary-bg rounded-3xl shadow-xl/15 shadow-border-shadow">
+                <span className="task-priority px-2 py-1 bg-secondary-bg rounded-3xl shadow-md shadow-border-shadow hover:text-primary-text">
                   {task.taskPriority}
                 </span>
-                <span className="task-category px-2 py-1 bg-secondary-bg rounded-3xl shadow-xl/15 shadow-border-shadow">
+                <span className="task-category px-2 py-1 bg-secondary-bg rounded-3xl shadow-md shadow-border-shadow hover:text-primary-text">
                   {task.taskCategory}
                 </span>
-                <span className="task-due-date px-2 py-1 bg-secondary-bg rounded-3xl shadow-xl/15 shadow-border-shadow">
+                <span className="task-due-date px-2 py-1 bg-secondary-bg rounded-3xl shadow-md shadow-border-shadow hover:text-primary-text">
                   {task.taskDueDate}
                 </span>
               </div>
